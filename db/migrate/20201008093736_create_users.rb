0b63549f0d7e40de14f0bdf0a2395ea3efa9b79e
inpachi-yume <itsuki.w.0606@gmail.com>
2020-10-08 19:22:57 +0900

    usersテーブルの作成

---
+++ db/migrate/20201008093736_create_users.rb
@@ -0,0 +1,16 @@
+class CreateUsers < ActiveRecord::Migration[6.0]
+  def change
+    create_table :users do |t|
+      t.string :nickname,         null: false
+      t.string :email,            null: false,uniquness:true
+      t.string :password,         null: false,uniquness:true
+      t.string :last_name,        null: false
+      t.string :first_name,       null: false
+      t.string :last_name_kana,   null: false
+      t.string :first_name_kana,  null: false
+      t.datetime :birth_date,     null: false
+
+      t.timestamps
+    end
+  end
+end
