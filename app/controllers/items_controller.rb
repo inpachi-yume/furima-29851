044ceb9fb07ec775dd60bce6801a4cd348fb226c
inpachi-yume <itsuki.w.0606@gmail.com>
2020-10-17 19:17:56 +0900

    ルーティングとコントローラーの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,18 @@
 class ItemsController < ApplicationController
+  def index
+  end
+  
+  
+  def new
+    @item = Item.new
+  end
+
+  def create
+    Item.create(item_params)
+  end
+
+  private
+
+  def item_params
+    params.require(item).parmit(:user, :name, :description, :category_id, :condition_id, :postage_payer_id, :prefecture_id, :shipping_time_id, :price)
 end
