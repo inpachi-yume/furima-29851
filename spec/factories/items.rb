4baf7a37cd0beb69ae6a560f818411b63d30b985
inpachi-yume <itsuki.w.0606@gmail.com>
2020-10-20 19:45:09 +0900

    ''を外す

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -2,12 +2,12 @@ FactoryBot.define do
   factory :item do
     name                {'バイク'}
     description         {'よく走ります！'}
-    category_id         {'2'}
-    condition_id        {'3'}
-    postage_payer_id    {'2'}
-    shipping_time_id    {'2'}
-    prefecture_id       {'2'}
-    price               {'300000'}
+    category_id         {2}
+    condition_id        {3}
+    postage_payer_id    {2}
+    shipping_time_id    {2}
+    prefecture_id       {2}
+    price               {300000}
     association :user
 
   end
