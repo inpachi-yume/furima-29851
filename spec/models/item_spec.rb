3ce4c7647ac7844384efb6893291ffe3110326c2
inpachi-yume <itsuki.w.0606@gmail.com>
2020-10-20 19:54:08 +0900

    異常系に other than の追加

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -73,6 +73,31 @@ RSpec.describe Item, type: :model do
         @item.price = '1000000'
         expect(@item.errors.full_messages).to include()
       end
+      it 'カテゴリーがーー状態だと登録出来ない' do
+        @item.category_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category must be other than 1")
+      end
+      it '商品の状態がーーだと登録出来ない' do
+        @item.condition_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Condition must be other than 1")
+      end
+      it '配送料の負担者がーーだと登録出来ない' do
+        @item.postage_payer_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Postage payer must be other than 1")
+      end
+      it '配送時間がーーだと登録出来ない' do
+        @item.shipping_time_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Shipping time must be other than 1")
+      end
+      it '発送地域がーーだと登録出来ない' do
+        @item.prefecture_id = 0
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Prefecture must be other than 0")
+      end
     end
   end
 end
