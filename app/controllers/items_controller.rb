d7e475189689d76958312794b412554c23f3a755
inpachi-yume <itsuki.w.0606@gmail.com>
2020-10-23 20:36:50 +0900

    商品編集、保存、エラーメッセージの実装。

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -25,7 +25,16 @@ class ItemsController < ApplicationController
   def edit
     @item = Item.find(params[:id])
   end
-  
+
+  def update
+    @item = Item.find(params[:id])
+    @item.update(item_params)
+    if @item.save
+      redirect_to item_path(@item)
+    else
+      render :edit
+    end
+  end
   private
 
   def item_params
