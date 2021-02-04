f73220f7cf5ce388933315798f3f21ab1da6e052
inpachi-yume <itsuki.w.0606@gmail.com>
2021-02-04 13:37:39 +0900

    フォームオブジェクトの修正

--- app/models/user_purchase.rb
+++ app/models/user_purchase.rb
@@ -6,7 +6,6 @@ class UserPurchase
     validates :item_id
     validates :user_id
     validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/ }
-    # \A\d{3}[-]\d{4}\z
     validates :prefecture_id, numericality: { other_than: 0 }
     validates :city
     validates :house_number
