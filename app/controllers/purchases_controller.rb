5277294d8a4bebf686b490fbc07128d10ef1fd18
inpachi-yume <itsuki.w.0606@gmail.com>
2021-02-04 17:49:28 +0900

    purchases_controller.rbの修正

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,7 +1,7 @@
 class PurchasesController < ApplicationController
   before_action :set_item, only: [:index, :create]
-  before_action :move_to_index, only: [:index]
-  before_action :authenticate_user!, only: [:index]
+  before_action :move_to_index, only: [:index, :cerate]
+  before_action :authenticate_user!, only: [:index, :cerate]
   def index
     @purchase = UserPurchase.new
   end
@@ -9,14 +9,7 @@ class PurchasesController < ApplicationController
   def create
     @purchase = UserPurchase.new(purchase_params)
     if @purchase.valid?
-      Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
-      Payjp::Charge.create(
-        amount: @item.price,  
-        card: purchase_params[:token],    
-        currency: 'jpy'                 
-      )
-      @purchase.save
-      redirect_to root_path(@item)
+      payment
     else
       render :index
     end
@@ -44,8 +37,19 @@ class PurchasesController < ApplicationController
 
   def move_to_index
     if user_signed_in? && current_user.id == @item.user_id or @item.purchase.present?
-      redirect_to root_path(@item)
+      redirect_to root_path
     end
+  end
+
+  def payment
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: @item.price,  
+      card: purchase_params[:token],    
+      currency: 'jpy'                 
+    )
+    @purchase.save
+    redirect_to root_path
   end
 
 end
