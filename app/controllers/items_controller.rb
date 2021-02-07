d5c0578778b0b497081c68939a90584ee2f266e4
inpachi-yume <itsuki.w.0606@gmail.com>
2021-02-07 15:22:09 +0900

    リダイレクトの修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create, :edit]
   before_action :set_item, only: [:show, :edit, :update, :destroy]
   before_action :move_to_index, only: [:edit, :update, :destroy]
   def index
@@ -41,7 +41,7 @@ class ItemsController < ApplicationController
   end
 
   def move_to_index
-    unless user_signed_in? && current_user.id == @item.user_id
+    unless user_signed_in? && current_user.id == @item.user_id && @item.purchase.nil?
       redirect_to action: :index
     end
   end
